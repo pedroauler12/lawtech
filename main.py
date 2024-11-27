ff87fe95cffb91eb39b8b53f022b100b0c4a2944
Pedro <Pedro.martins@sou.inteli.edu.br>
2024-11-27 12:14:03 -0300

    abrir cada materia de uma vez

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import os
 from dotenv import load_dotenv
-import time 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 load_dotenv()
 
@@ -23,19 +25,51 @@ driver = webdriver.Chrome(service=service, options=options)
 url =  "https://scon.stj.jus.br/SCON/pesquisa_pronta/listaPP.jsp"
 
 driver.get(url)
-time.sleep(5)
 
-btn_abrir_todas = driver.find_element(By.ID, "btnAbrirTodas")
-btn_abrir_todas.click()
+
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "btnAbreMateria")))
+
+# Localizar todos os botões das matérias
+botoes_materias = driver.find_elements(By.CLASS_NAME, "btnAbreMateria")
+
+for botao in botoes_materias:
+    try:
+        # Verificar se a matéria já está expandida
+        if botao.get_attribute("aria-expanded") == "true":
+            print(f"Matéria {botao.get_attribute('data-bs-target')} já está expandida. Pulando...")
+            continue
+
+        # Rolar para o botão estar visível (evita problemas de clique)
+        driver.execute_script("arguments[0].scrollIntoView(true);", botao)
+        time.sleep(1)  # Pausa breve para estabilizar o scroll
+
+        # Usar JavaScript para clicar no botão
+        driver.execute_script("arguments[0].click();", botao)
+        print(f"Matéria {botao.get_attribute('data-bs-target')} foi aberta.")
+
+        # Pausa para permitir a abertura da matéria
+        time.sleep(2)
+
+    except Exception as e:
+        print(f"Erro ao processar o botão: {e}")
+
+# temos o id div de cada area do direito , sendo id="divMateria*" onde * é um numero de 1 a 13
+
+'''
+time.sleep(2)
+materia = driver.find_element(By.ID, "divMateria1")
+print(materia.text)
+
+
 
 # Capturar o elemento de interesse
-time.sleep(2)  # Aguardar o carregamento do conteúdo
-material = driver.find_element(By.ID, "divTitulo10")
+
+materia = driver.find_element(By.ID, "divTitulo10")
 
 # Obter o texto visível
 print("Texto visível no elemento:")
-print(material.text)
-
-
+print(materia.text)
+'''
 
 driver.quit()
